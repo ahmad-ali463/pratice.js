fa33b5685420d1bfa09d6378a5e45d54a8c59a07
Ahmad Ali Asif <spyali463@gmail.com>
2023-03-25 11:56:35 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,29 @@
+// // write a program to a user that give your grade according to numbers
+
+var userscore =+ prompt("enter your number")
+if (userscore< 50){
+    console.log("your are fail");
+}
+ else if (userscore < 60){
+    console.log("your grade is D");
+}
+else if (userscore < 70 ){
+console.log(" your are grade is C");
+}
+ else if (userscore < 80 ){
+console.log("your are grade is B");
+}
+else if ( userscore < 90){
+    console.log("your grade is A");
+}
+else {
+    console.log("your grade is A+");
+}
+
+var inputnumber =+ prompt("Enter inpute number")
+if (inputnumber % 2 == 0 ){
+    console.log("the number is even");
+}
+else{
+    console.log("the number is odd");
+}
