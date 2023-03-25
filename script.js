8bab3ceefaa8c4853c558125d1a45927ba6d98c4
Ahmad Ali Asif <spyali463@gmail.com>
2023-03-25 14:38:33 +0500

    javascrpt

--- script.js
+++ script.js
@@ -1,29 +1,35 @@
-// // write a program to a user that give your grade according to numbers
+// // // write a program to a user that give your grade according to numbers
 
-var userscore =+ prompt("enter your number")
-if (userscore< 50){
-    console.log("your are fail");
-}
- else if (userscore < 60){
-    console.log("your grade is D");
-}
-else if (userscore < 70 ){
-console.log(" your are grade is C");
-}
- else if (userscore < 80 ){
-console.log("your are grade is B");
-}
-else if ( userscore < 90){
-    console.log("your grade is A");
-}
-else {
-    console.log("your grade is A+");
-}
+// var userscore =+ prompt("enter your number")
+// if (userscore< 50){
+//     console.log("your are fail");
+// }
+//  else if (userscore < 60){
+//     console.log("your grade is D");
+// }
+// else if (userscore < 70 ){
+// console.log(" your are grade is C");
+// }
+//  else if (userscore < 80 ){
+// console.log("your are grade is B");
+// }
+// else if ( userscore < 90){
+//     console.log("your grade is A");
+// }
+// else {
+//     console.log("your grade is A+");
+// }
 
-var inputnumber =+ prompt("Enter inpute number")
-if (inputnumber % 2 == 0 ){
-    console.log("the number is even");
-}
-else{
-    console.log("the number is odd");
-}
+// var inputnumber =+ prompt("Enter inpute number")
+// if (inputnumber % 2 == 0 ){
+//     console.log("the number is even");
+// }
+// else{
+//     console.log("the number is odd");
+// }
+
+
+var Name = 2;
+var Name1 = 4;
+var Name2 = 6;
+console.log(Name+Name1+Name2);
